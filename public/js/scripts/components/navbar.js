381d829656da1dd1ca4cdff8733e8cdf7943e163
Nikolaidk3 <hp82fk@student.aau.dk>
2025-03-19 12:48:12 +0100

    footer og navbar component tilføjet
    med styling

---
+++ public/js/scripts/components/navbar.js
@@ -0,0 +1,10 @@
+function loadNavbar() {
+    fetch("/public/components/navbar.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("navbar").innerHTML = html;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+}
+
+document.addEventListener("DOMContentLoaded", loadNavbar);
