1bda2fcce367267246d5101e3fc94b98d6b41965
Nikolaidk3 <hp82fk@student.aau.dk>
2025-03-21 08:51:30 +0100

    hashing af password

--- public/js/login/register.js
+++ public/js/login/register.js
@@ -11,5 +11,38 @@ document.getElementById("signup").addEventListener("submit", function(e) {
     }
 
     alert("Du er nu registreret og kan logge ind");
+    
     window.location.href ="/public/pages/login/login.html";
 })
+
+//udkast til at gemme email og password i mySQL
+/*
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+function addUser(data) {
+    //hashing password
+    const passwordhashed = bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) throw err;
+        })
+    })
+
+    let query = mysql.format(insertQuery, ["email", "password", data.email, data.passwordhashed]);
+    Pool.query(query, (err, response) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log(response, insertId);
+    })
+} 
+
+setTimeout(() => {
+    addUser({
+        "email": data.email,
+        "password": data.passwordhashed
+    })
+})
+    */
