2f494e0f7172a2d3f45fa5021b85a4cea343dfe5
Nicolai-Schultz <nicoschultz42@gmail.com>
2025-04-02 09:28:34 +0200

    Progress with csv

--- public/js/admin/addproducts.js
+++ public/js/admin/addproducts.js
@@ -1,3 +1,14 @@
 import { loadComponents } from "./loadcomponents/loadcomponents.js";
 
 loadComponents();
+
+function onLoad() {
+    fetch("/public/js/admin/TestCsv.csv")
+        .then((res) => res.text())
+        .then((data) => {
+            let splitData = data.split(";",3);
+            console.log(splitData);
+        });
+}
+
+onLoad();
