9bca2931138ddddaa53ffed4ec9c206f044e2cec
JonatanDZ <jmuhle24@student.aau.dk>
2025-04-04 14:19:42 +0200

    Recommender algo færdig

--- server/recommender/rec.js
+++ server/recommender/rec.js
@@ -25,6 +25,13 @@ function fetchData(callback) {
       const data = results.map(row => Object.values(row));
       callback(data);
     });
+    db.end(err => {
+        if (err) {
+            console.error('Error closing the database connection:', err);
+        } else {
+            console.log('Database connection closed.');
+        };
+    });
 }
 
 function dotProduct(user, item) {
@@ -43,20 +50,25 @@ function resultsComparedPrinted(resultsCompared) {
 
 //https://www.w3schools.com/js/js_array_sort.asp#mark_sort Se hvordan det virker her.
 function compareLists(results) {
-    return results.sort(function(a, b){return b - a});
+    return results.sort(function(a, b){return b.score - a.score});
 }
 
 function recommendedItem(user, numberOfLists) {
-    let resultsOfDotProduct = [];
-    for (let list of numberOfLists) { 
-        resultsOfDotProduct.push(dotProduct(user, list)); 
-    }    
+    let resultsOfDotProduct = numberOfLists.map(list => ({
+        id: list[0], 
+        score: dotProduct(user, list.slice(1))
+    }));
     
     let resultsCompared = compareLists(resultsOfDotProduct);
+    //Giver nummer et recommended
+    console.log("Detter er nummer 1 recommended: ", resultsCompared[0]);
+    console.log()
+    //giver hele listen i recommended rækkefølge
     resultsComparedPrinted(resultsCompared);
 }
 
-let user = [0, 0, 0];
+//Test user
+let user = [0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0];
 
 fetchData((data) => {
     recommendedItem(user, data);    
