1b9d566fed2878aad514712f785f52bf5b2ba7e8
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2025-04-08 08:54:12 +0200

    updated reccomender

--- server/event_recommender.js
+++ server/event_recommender.js
@@ -1,31 +1,25 @@
-let user_array = [];
+let count = [0, 0, 0, 0, 0];
 
 let data = [
-  [0, 1, 1],
-  [1, 0, 1],
-  [1, 1, 0],
-  [0, 1, 1],
+  [0, 1, 1, 1, 0],
+  [1, 0, 1, 1, 1],
+  [1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1],
 ];
 
-let user = [0, 0, -1];
+let user = [0, 0, -1, 1, -1];
 
-for (let j = 0; j < 4; j++) {
-  if (user[j] != -1) {
-    for (let i = 0; i < data.length; i++) {
-      if ((data[i][j] = user[j])) {
-        user_array.append(data[i][user.indexOf(-1)]);
+for (let j = 0; j < user.length; j++) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][j] == user[j]) {
+      for (let n = 0; n < user.length; n++) {
+        if (user[n] == -1) {
+          if (data[i][n] == 1) {
+            count[n] += 1;
+          } else count[n] -= 1;
+        }
       }
     }
-  }
-}
-
-let count = 0;
-//Count amount of 1's and 0's in user_array
-for (let i = 0; i < user_array.length; i++) {
-  if (user_array[i] == 1) {
-    count += 1;
-  } else if (user_array[i] == 0) {
-    count -= 1;
   }
 }
 
