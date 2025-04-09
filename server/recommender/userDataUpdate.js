8badb12bd6c760eb5b36cc29b2c94339388c9ac1
JonatanDZ <jmuhle24@student.aau.dk>
2025-04-09 11:20:50 +0200

    Opdateret med db, for item

--- server/recommender/userDataUpdate.js
+++ server/recommender/userDataUpdate.js
@@ -1,3 +1,36 @@
+const mysql = require("mysql2/promise");
+
+async function fetchData(id) {
+    let connection;
+    try {
+        // Create connection
+        connection = await mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "StrongP@ssw0rd!",
+            database: "myDB",
+            port: 3306
+        });
+
+        console.log("Connected to MySQL");
+
+        // Fetch clothing items
+        let itemId = id;
+        const [itemIdData] = await connection.execute('SELECT * FROM clothing_items WHERE id = ?', [itemId]);
+        const itemIdDataFixed = itemIdData.map(row => Object.values(row));
+
+
+        return itemIdDataFixed;
+
+    } catch (err) {
+        console.error("Database error:", err);
+    } finally {
+        if (connection) {
+            await connection.end();
+            console.log("Database connection closed.");
+        }
+    }
+}
 
 item_id = [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0];
 user_id = [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0];
@@ -14,12 +47,15 @@ function insertData(item_id, user_id) {
 //mangler funktion der henter user ned, og manglr en funktion der får item. 
 
 // Forside skal laves først
-
+/* 
 document.getElementById().addEventListener("click", function() {
     let insertData(item_id, user_id);
     //skal ind og rette db efter opdatering. 
     alt table db.userUpdate(insertData);
 
-});
+}); */
 
-//console.log(user_id);
+let id = 5;
+fetchData(id).then(result => {
+    console.log("Result:", result);
+});
