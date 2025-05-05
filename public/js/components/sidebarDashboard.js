5c4e0de561fdae59f08f613ce7b25f9d512c4332
Nikolaidk3 <hp82fk@student.aau.dk>
2025-05-05 12:31:32 +0200

    kommentarer til login, signup og authentication

--- public/js/components/sidebarDashboard.js
+++ public/js/components/sidebarDashboard.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
+    // If no token return (not running the rest of the code)
     if (!token) return;
 
+
     try {
+        // Send a POST request to verify-token (happens in router.js)
         const response = await fetch('/verify-token', {
             method: 'POST',
             headers: {
@@ -12,12 +15,14 @@ document.addEventListener('DOMContentLoaded', async () => {
             body: JSON.stringify({ token })
         });
 
+        // Parsing JSON response
         const data = await response.json();
 
         const sidebarLinks = document.getElementById('sidebar-links');
 
         if (!sidebarLinks) return; // Safety check
 
+        // Checks if the user is authenticated AND admin, and shows the admin button (gets it from verify-token)
         if (data.isAuthenticated && data.isAdmin) {
             const adminLink = document.createElement('li');
             adminLink.classList.add('nav-item');
@@ -25,11 +30,13 @@ document.addEventListener('DOMContentLoaded', async () => {
             sidebarLinks.prepend(adminLink);
         }
 
+        // Creating and adding logout link
         const logoutLink = document.createElement('li');
 
         logoutLink.innerHTML = `<a class="nav-link text-danger" href="#" id="logout-link">Log ud</a>`;
         sidebarLinks.appendChild(logoutLink);
 
+        // When logout link is clicked it removes token from localStorage
         document.getElementById('logout-link').addEventListener('click', (e) => {
             e.preventDefault();
             localStorage.removeItem('token'); // Delete token
