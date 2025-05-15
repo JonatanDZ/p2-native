681ac7992bac2bc6cd4fe762c91c1777fa35ea85
FreshLuna <msanda24@student.aau.dk>
2025-05-15 12:36:11 +0200

    fixed and removed code
    ding dong

--- public/js/components/sidebarEvent.js
+++
@@ -1,9 +0,0 @@
-async function loadSidebar(){
-    await fetch('/public/pages/events/sidebarEvents.html')
-    .then(res => res.text())
-    .then(html => {
-        document.getElementById("sidebar-container").innerHTML = html;
-
-    });
-}
-document.addEventListener("DOMContentLoaded", loadSidebar);
--- public/js/components/sidebarProducts.js
+++
@@ -1,9 +0,0 @@
-async function loadSidebar(){
-    await fetch('/public/pages/products/sidebarProducts.html')
-    .then(res => res.text())
-    .then(html => {
-        document.getElementById("sidebar-container").innerHTML = html;
-
-    });
-}
-document.addEventListener("DOMContentLoaded", loadSidebar);
