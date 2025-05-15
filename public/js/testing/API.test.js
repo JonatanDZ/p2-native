cce4e9c0a64fe377335266cadd00d4e43170ecc6
Nicolai-Schultz <nicoschultz42@gmail.com>
2025-05-15 14:18:29 +0200

    more tests

--- public/js/testing/API.test.js
+++ public/js/testing/API.test.js
@@ -28,3 +28,55 @@ test("GET /get-events returns event list", async () => {
   expect(Array.isArray(res.body)).toBe(true);
 });
 
+test("GET /get-event returns single event", async () => {
+  // assuming that there is an id = 10, if not will fail. 
+  const res = await request(server).get("/get-event?id=10");
+
+  expect(res.statusCode).toBe(200);
+  expect(typeof res.body).toBe("object");
+});
+
+test("GET /get-product returns single event", async () => {
+  // assuming that there is an id = 569, if not will fail. 
+  const res = await request(server).get("/get-product?id=569");
+
+  expect(res.statusCode).toBe(200);
+  expect(typeof res.body).toBe("object");
+});
+
+// POST requests:
+// TODO: change login to correct one given mock data
+test("POST posting a login returns success and JWT token", async () => {
+  const response = await request(server)
+    .post("/login")
+    .send({
+      email: "pikken@gmail.com",
+      password: "123"
+    })
+    .set("Content-Type", "application/json");
+
+  // asserting that the server responds success
+  expect(response.statusCode).toBe(200);
+
+  // asserting that JSON response has a message and token
+  expect(response.body).toHaveProperty("message");
+  expect(response.body).toHaveProperty("token");
+  expect(typeof response.body.token).toBe("string");
+});
+
+/*
+test('', async ()=>{
+  const response = await request(server)
+    .post("/save-events")
+    .send({
+      price: "99",
+      place: "Aalborg",
+      picture: "event_09.jpg",
+      info: "Test",
+      name: "testname",
+      time: "testtime"
+    })
+    .set("Content-Type", "application/json");
+
+    expect(response.statusCode).toBe(200);
+}) */
